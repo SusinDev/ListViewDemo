8f0bc0f96b67d081fc62edac2d2fa8cbd569d193
HappyPikachu <785237230@qq.com>
2016-05-10 00:07:32 +0800

    base interface

--- app/src/main/java/com/example/white/listviewdemo/activity/MainActivity.java
+++ app/src/main/java/com/example/white/listviewdemo/activity/MainActivity.java
@@ -1,23 +1,30 @@
 package com.example.white.listviewdemo.activity;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.example.white.listviewdemo.R;
-import com.example.white.listviewdemo.model.MenuItem;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<MenuItem> items = new ArrayList<MenuItem>();
+    Toolbar mToolbar;
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
 
+        setSupportActionBar(mToolbar);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar
+                , R.string.app_name, R.string.app_name);
+        mDrawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
     }
 
 
