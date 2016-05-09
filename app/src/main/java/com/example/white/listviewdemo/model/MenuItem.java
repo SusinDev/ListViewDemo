b0e5cf005cac56f821a8c4edd316da9661d97d2f
HappyPikachu <785237230@qq.com>
2016-05-10 00:09:03 +0800

    base interface

---
+++ app/src/main/java/com/example/white/listviewdemo/model/MenuItem.java
@@ -0,0 +1,23 @@
+package com.example.white.listviewdemo.model;
+
+/**
+ * Created by white on 2016/5/9.
+ */
+public class MenuItem {
+
+    private String id;
+
+    private String title;
+
+
+    public void setId(String id) { this.id = id; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getId() { return id; }
+
+}
