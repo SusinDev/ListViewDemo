39570b48ce77b727566b10815ddfdaf50d3759a1
HappyPikachu <785237230@qq.com>
2016-05-09 21:48:14 +0800

    change

---
+++ app/src/main/java/com/example/white/listviewdemo/activity/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.white.listviewdemo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.white.listviewdemo.R;
+import com.example.white.listviewdemo.model.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<MenuItem> items = new ArrayList<MenuItem>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
