689ddabb7d8142fd6c54a195b137eeadb942d267
HappyPikachu <785237230@qq.com>
2016-05-10 09:24:53 +0800

    simpleadapter

--- app/src/main/java/com/example/white/listviewdemo/fragment/MenuFragment.java
+++ app/src/main/java/com/example/white/listviewdemo/fragment/MenuFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.white.listviewdemo.R;
 
@@ -13,10 +15,18 @@ import com.example.white.listviewdemo.R;
  */
 public class MenuFragment extends Fragment {
 
+    private ListView mListView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        mListView = (ListView) view.findViewById(R.id.listview_menu);
+        //为菜单项准备好数据，用的是ArrayAdapter的方式绑定ListView
+        String[] menuItem = {"SimpleAdapter的方式", "BaseAdapter的方式"};
+        ArrayAdapter<String> menu = new ArrayAdapter<String>(getActivity(),
+                                    android.R.layout.simple_list_item_1,menuItem);
+        mListView.setAdapter(menu);
         return view;
     }
 }
